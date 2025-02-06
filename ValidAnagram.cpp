282e0d196fa04294a7edefedff626a5263d4ecae
Ramdani <dannymld41@gmail.com>
2025-02-06 15:32:37 +0700

    valid Anagram Solution

---
+++ ValidAnagram.cpp
@@ -0,0 +1,19 @@
+bool isAnagram(string s, string t) {
+        if(s.length() != t.length()){
+            return false;
+        }else{
+            bool isAnagram = false;
+            sort(s.begin(),s.end());
+            sort(t.begin(),t.end());
+            for(int i = 0; i < s.length(); i++){
+                if(s[i] != t[i]){
+                    isAnagram = false;
+                    break;
+                }else{
+                    isAnagram = true;
+                }
+            }
+            return isAnagram;
+                
+        }
+    }
